7a8f0cda1378f0f106e1ed6ae6d04cad4926d96d
Alin Marin Elena <alin@elena.space>
2022-07-03 09:56:33 +0100

    update 8x16

--- keyboards/handwired/ortho8x16/rev1/rules.mk
+++ keyboards/handwired/ortho8x16/rev1/rules.mk
@@ -2,8 +2,9 @@
 MCU = STM32F401
 
 # Bootloader selection
-#BOOTLOADER = stm32-dfu
-BOOTLOADER = tinyuf2
+BOOTLOADER = stm32-dfu
+
+#BOOTLOADER = tinyuf2
 # Build Options
 #   change yes to no to disable
 #
