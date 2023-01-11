b7979935f59cf962528fc3de39d86d35959b529b
elpekenin <elpekenin@elpekenin.dev>
2023-01-11 23:00:19 +0100

    Avoid useless code on SIPO

--- users/elpekenin/registers/sipo_pins.c
+++ users/elpekenin/registers/sipo_pins.c
@@ -1,19 +1,26 @@
 // Copyright 2023 Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "print.h"
 #include "sipo_pins.h"
 #include "custom_spi_master.h"
-#include "wait.h"
 
 #define REGISTER_SPI_DRIVER_ID 1
 
 uint8_t register_pin_state[_REGISTER_BYTES] = {[0 ... _REGISTER_BYTES-1] = 0};
+bool register_state_changed = true;
 
 void set_register_pin(uint8_t  position, bool state) {
     uint8_t byte_offset = position / 8;
     uint8_t bit_offset  = position % 8;
 
+    // Check if pin already had that state
+    uint8_t curr_value = (register_pin_state[byte_offset] >> bit_offset) & 1;
+    if (curr_value == state) {
+        sipo_dprintf("set_register_pin: bit already had the desired value\n");
+        return;
+    }
+
+    register_state_changed = true;
     if (state)
         // add data starting on the least significant bit
         register_pin_state[byte_offset] |=  (1 << bit_offset);
@@ -22,10 +29,17 @@ void set_register_pin(uint8_t  position, bool state) {
 }
 
 void write_register_state() {
+    if (!register_state_changed) {
+        sipo_dprintf("write_register_state: the value on the register(s) is already set\n");
+        return;
+    }
+
+    register_state_changed = false;
+
     custom_spi_init(REGISTER_SPI_DRIVER_ID);
 
     if(!custom_spi_start(REGISTER_CS_PIN, false, REGISTER_SPI_MODE, REGISTER_SPI_DIV, REGISTER_SPI_DRIVER_ID)) {
-        printf("Couldn't start SPI for SIPO\n");
+        sipo_dprintf("Couldn't start SPI for SIPO\n");
         return;
     }
 
