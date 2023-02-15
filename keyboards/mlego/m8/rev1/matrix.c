38f0a2ff245860916cd9221f8dbe12ae17093f54
Alin Marin Elena <alin@elena.space>
2023-02-15 19:27:49 +0000

    update

--- keyboards/mlego/m8/rev1/matrix.c
+++ keyboards/mlego/m8/rev1/matrix.c
@@ -35,12 +35,12 @@ static inline void shift_out(uint16_t value) {
 
   uint8_t message[2]  = {(value >> 8) & 0xFF ,(uint8_t)(value & 0xFF) };
 
-  //writePinLow(SPI_LATCH_PIN);
+ // writePinLow(SPI_LATCH_PIN);
   spi_start(SPI_LATCH_PIN, true, 3, SPI_DIVISOR);
   spi_transmit(message,2);
   spi_stop();
-//  writePinHigh(SPI_LATCH_PIN);
-  matrix_output_select_delay();
+  //writePinHigh(SPI_LATCH_PIN);
+  //matrix_output_select_delay();
 
 }
 
@@ -59,7 +59,7 @@ void matrix_init_custom(void) {
 
   setPinOutput(SPI_LATCH_PIN);
   matrix_io_delay();
-  //spi_start(SPI_LATCH_PIN, true, 3, SPI_DIVISOR);
+ // spi_start(SPI_LATCH_PIN, true, 3, SPI_DIVISOR);
   matrix_io_delay();
 }
 
