ce6aac82e1356c9e3eb412a743e543fbcfd907f9
elpekenin <elpekenin@elpekenin.dev>
2023-03-04 22:56:09 +0100

    Sorting

--- drivers/painter/eink_panel/qp_eink_panel.c
+++ drivers/painter/eink_panel/qp_eink_panel.c
@@ -9,9 +9,62 @@
 #include "qp_surface_internal.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Quantum Painter API implementations
+// Helper functions
+
+// Reset can_flush flag back to true after timeout
+uint32_t can_flush_callback(uint32_t trigger_time, void *cb_arg) {
+    eink_panel_dc_reset_painter_device_t *driver = (eink_panel_dc_reset_painter_device_t *)cb_arg;
+
+    driver->can_flush = true;
+
+    return 0;
+}
+
+// Set can_flush to false and schedule its cleanup
+void qp_eink_update_can_flush(painter_device_t device) {
+    eink_panel_dc_reset_painter_device_t *driver  = (eink_panel_dc_reset_painter_device_t *)device;
+
+    driver->can_flush = false;
+    defer_exec(driver->timeout, can_flush_callback, (void *)device);
+}
+
+static inline void decode_masked_pixels(uint8_t *pixels, uint32_t byte, uint8_t *black, uint8_t *red) {
+    /* Convert pixel data into convenient representation, buffer holds data as:
+     * B1 R1  B2 R2  B3 R3  B4 R4 || B5 R5  B6 R6  B7 R7  B8 R8 || ...
+     *
+     * This function shuffles it so we get
+     *   black_data: B8 B7 B6 B5 B4 B3 B2 B1
+     *   red_data:   R8 R7 R6 R5 R4 R3 R2 R1
+     */
+    uint16_t raw_data = (pixels[byte] << 8) | (pixels[byte + 1]);
+
+    // clear data so we can simply |=
+    *black = 0;
+    *red   = 0;
+
+    uint16_t black_mask = 1 << 15;
+    uint16_t red_mask   = 1 << 14;
+
+    for (uint8_t i = 0; i < 8; ++i) {
+        bool black_bit = raw_data & black_mask;
+        bool red_bit   = raw_data & red_mask;
+
+        black_mask >>= 2;
+        red_mask   >>= 2;
+
+        *black |= (black_bit << i);
+        *red   |= (red_bit   << i);
+    }
+}
+
+// Interpolate any HSV888 to the nearest of the three
+static inline uint32_t hsv_distance(uint8_t h, uint8_t s, uint8_t v, HSV hsv) {
+    return (h - hsv.h) * (h - hsv.h) + (s - hsv.s) * (s - hsv.s) + (v - hsv.v) * (v - hsv.v);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Quantum Painter API implementations
+
 // Power control
 bool qp_eink_panel_power(painter_device_t device, bool power_on) {
     painter_driver_t *                           driver = (painter_driver_t *)device;
@@ -22,7 +75,6 @@ bool qp_eink_panel_power(painter_device_t device, bool power_on) {
     return true;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Screen clear
 bool qp_eink_panel_clear(painter_device_t device) {
     // this function gets called by eink's init
@@ -45,25 +97,6 @@ bool qp_eink_panel_clear(painter_device_t device) {
     return true;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Reset can_flush flag back to true after timeout
-uint32_t can_flush_callback(uint32_t trigger_time, void *cb_arg) {
-    eink_panel_dc_reset_painter_device_t *driver = (eink_panel_dc_reset_painter_device_t *)cb_arg;
-
-    driver->can_flush = true;
-
-    return 0;
-}
-
-// Set can_flush to false and schedule its cleanup
-void qp_eink_update_can_flush(painter_device_t device) {
-    eink_panel_dc_reset_painter_device_t *driver  = (eink_panel_dc_reset_painter_device_t *)device;
-
-    driver->can_flush = false;
-    defer_exec(driver->timeout, can_flush_callback, (void *)device);
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Screen flush
 bool qp_eink_panel_flush(painter_device_t device) {
     eink_panel_dc_reset_painter_device_t *       driver  = (eink_panel_dc_reset_painter_device_t *)device;
@@ -95,7 +128,6 @@ bool qp_eink_panel_flush(painter_device_t device) {
     return true;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Viewport to draw to
 bool qp_eink_panel_viewport(painter_device_t device, uint16_t left, uint16_t top, uint16_t right, uint16_t bottom) {
     eink_panel_dc_reset_painter_device_t *driver = (eink_panel_dc_reset_painter_device_t *)device;
@@ -106,57 +138,13 @@ bool qp_eink_panel_viewport(painter_device_t device, uint16_t left, uint16_t top
     return true;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Stream pixel data to the current write position
-static inline void decode_bitmask(uint8_t *pixels, uint32_t byte, uint8_t *black, uint8_t *red) {
-    /* Convert pixel data into convenient representation
-     *
-     * B1 R1  B2 R2  B3 R3  B4 R4 || B5 R5  B6 R6  B7 R7  B8 R8
-     * Becomes
-     * black_data: B8 B7 B6 B5 B4 B3 B2 B1
-     * red_data:   R8 R7 R6 R5 R4 R3 R2 R1
-     *
-     * Note: Will be grabbing 8 pixels at most, thus uint16_t is enough
-     * Note: Always accessing `byte + 1` might go out of the buffer if display's (w*h) % 8 != 0
-     */
-    uint16_t raw_data = (pixels[byte] << 8) | (pixels[byte + 1]);
-
-    // clear data so we can simply |=
-    *black = 0;
-    *red   = 0;
-
-    uint16_t black_mask = 1 << 15;
-    uint16_t red_mask   = 1 << 14;
-
-    for (uint8_t i = 0; i < 8; ++i) {
-        bool black_bit = raw_data & black_mask;
-        bool red_bit   = raw_data & red_mask;
-
-        black_mask >>= 2;
-        red_mask   >>= 2;
-
-        *black |= (black_bit << i);
-        *red   |= (red_bit   << i);
-    }
-}
-
 bool qp_eink_panel_pixdata(painter_device_t device, const void *pixel_data, uint32_t native_pixel_count) {
     eink_panel_dc_reset_painter_device_t *driver = (eink_panel_dc_reset_painter_device_t *)device;
     painter_driver_t *                    black  = (painter_driver_t *)driver->black_surface;
     painter_driver_t *                    red    = (painter_driver_t *)driver->red_surface;
     uint8_t *                             pixels = (uint8_t *)pixel_data;
 
-    /* By parsing 2 bytes at a time, we can make "buffers" of 8 pixels
-     * We do this &'ing and and shifting each bit to its position
-     *
-     *
-     * - 2 bytes (8 pixels) => Increse i by that amount
-     * - We don't have to compute a bit_offset on the loop's indexing
-     *   Will always land at the start of a byte
-     * - Since each byte stores 4 pixels, byte offset is i / 4
-     * - We don't want to grab data further down on the buffer
-     *   have to check if current index + 8 goes out of bounds
-     */
     uint32_t i = 0;
     uint8_t black_data, red_data;
     while (i < native_pixel_count) {
@@ -165,7 +153,7 @@ bool qp_eink_panel_pixdata(painter_device_t device, const void *pixel_data, uint
         uint8_t byte = i / 4;
 
         // stream data to display
-        decode_bitmask(pixels, byte, &black_data, &red_data);
+        decode_masked_pixels(pixels, byte, &black_data, &red_data);
         black->driver_vtable->pixdata(driver->black_surface, (const void *)&black_data, pixels_this_loop);
         red->driver_vtable->pixdata(driver->red_surface, (const void *)&red_data, pixels_this_loop);
 
@@ -176,12 +164,7 @@ bool qp_eink_panel_pixdata(painter_device_t device, const void *pixel_data, uint
     return true;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Convert supplied palette entries into their native equivalents
-static inline uint32_t hsv_distance(uint8_t h, uint8_t s, uint8_t v, HSV hsv) {
-    return (h - hsv.h) * (h - hsv.h) + (s - hsv.s) * (s - hsv.s) + (v - hsv.v) * (v - hsv.v);
-}
-
 bool qp_eink_panel_palette_convert(painter_device_t device, int16_t palette_size, qp_pixel_t *palette) {
     eink_panel_dc_reset_painter_device_t *driver = (eink_panel_dc_reset_painter_device_t *)device;
 
@@ -210,11 +193,9 @@ bool qp_eink_panel_palette_convert(painter_device_t device, int16_t palette_size
 
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Append pixels to the target location, keyed by the pixel index
 bool qp_eink_panel_append_pixels(painter_device_t device, uint8_t *target_buffer, qp_pixel_t *palette, uint32_t pixel_offset, uint32_t pixel_count, uint8_t *palette_indices) {
-    // data should end up arranged:
-    // B1R1B2R2  B3R3B4R4 | ...
+    // Data ends up arranged: B1R1B2R2  B3R3B4R4 | ...
     for (uint32_t i = 0; i < pixel_count; ++i) {
         uint32_t pixel_num = pixel_offset + i;
 
@@ -222,12 +203,15 @@ bool qp_eink_panel_append_pixels(painter_device_t device, uint8_t *target_buffer
         uint32_t byte_offset = pixel_num / 4;
         uint8_t  bit_offset  = 3 - (pixel_num % 4);
 
+        // check each color bit from palette
         bool black_bit = palette[palette_indices[i]].mono & 2;
         bool red_bit   = palette[palette_indices[i]].mono & 1;
 
+        // compute where data goes
         uint8_t black_mask = 1 << (2 * bit_offset + 1);
         uint8_t red_mask   = 1 << (2 * bit_offset + 0);
 
+        // add it
         if (black_bit)
             target_buffer[byte_offset] |= black_mask;
         else
