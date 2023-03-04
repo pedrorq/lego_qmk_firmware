43867b7d8ef167c35f7060c0610cea4e6e3acbca
Alin Marin Elena <alin@elena.space>
2023-03-04 21:34:02 +0000

    now works

--- drivers/painter/ssd1680/qp_ssd1680.c
+++ drivers/painter/ssd1680/qp_ssd1680.c
@@ -52,10 +52,10 @@ bool qp_ssd1680_init(painter_device_t device, painter_rotation_t rotation) {
     const uint8_t ssd1680_init_sequence[] = {
         // Command,                       Delay, N, Data[0],Data[1],...,Data[N-1]
         SSD1680_SOFT_RESET                    , 250 , 0   , //0x12
-        SSD1680_DRIVER_OUTPUT_CONTROL         , 250 , 3   , y_lsb, y_msb, 0x00, //0x01
+        SSD1680_DRIVER_OUTPUT_CONTROL         , 250 , 3   , 0x27, 0x01, 0x00, //0x01
         SSD1680_DATA_ENTRY_MODE               , 0   , 1   , 0x03, //0x11
         SSD1680_RAM_X_SIZE                    , 0   , 2   , 0x00, x   , //0x44
-        SSD1680_RAM_Y_SIZE                    , 0   , 2   , 0x00, 0x00, 0x00, (driver->base.panel_height) &0xFF, //0x45
+        SSD1680_RAM_Y_SIZE                    , 0   , 4   , 0x00, 0x00, 0x00, (driver->base.panel_height) &0xFF, //0x45
         SSD1680_BORDER_CONTROL                , 0   , 1   , 0x05, //0x3C
         SSD1680_TEMP_SENSOR                   , 0   , 1   , 0x80, //0x18
         SSD1680_WRITE_VCOM_REGISTER           , 0   , 1   , 0x70, //0x2C
@@ -76,7 +76,7 @@ bool qp_ssd1680_init(painter_device_t device, painter_rotation_t rotation) {
   0x0 , 0x0 , 0x0 , 0x0 , 0x0 , 0x0 , 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
   0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x0, 0x0, 0x0, //0x32
         SSD1680_RAM_X_COUNTER                 , 0   , 1   , 0x00, //0x4E
-        SSD1680_RAM_Y_COUNTER                 , 0   , 2   , y_lsb, y_msb, //0x4F
+        SSD1680_RAM_Y_COUNTER                 , 0   , 2   , 0x00, 0x00, //0x4F
         SSD1680_DISPLAY_UPDATE_CONTROL        , 250 , 1   , 0xCF, //0x22
         SSD1680_ACTIVATE_DISPLAY_UPDATE       , 250 , 0   , //0x20
 
