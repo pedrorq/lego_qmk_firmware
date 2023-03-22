5696e248c5dd365021c1e8b0b6ed4b01d141bbdd
alin m elena <alinm.elena@gmail.com>
2023-03-22 12:46:08 +0000

    update tipro

--- keyboards/tipro/keymaps/default_ansiortho/keymap.c
+++ keyboards/tipro/keymaps/default_ansiortho/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_1   , KC_2    , KC_3   , KC_4    , KC_5  , KC_6  , KC_7  , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_PSLS, KC_PAST,
     KC_TAB , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T  , KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_P7  , KC_P8  , KC_P9  ,
     KC_NUHS, KC_A   , KC_S    , KC_D   , KC_F    , KC_G  , KC_H  , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_P4  , KC_P5  , KC_P6  ,
-    KC_LSPO, KC_NUHS, KC_Z    , KC_X   , KC_C    , KC_V  , KC_B  , KC_N  , KC_M   , KC_COMM, KC_DOT , KC_UP  , KC_SLSH, KC_P1  , KC_P2  , KC_P4  ,
-    KC_LCTL, KC_LGUI, TT(_LWR), KC_LALT, TT(_RSE), KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_APP , KC_P0  , KC_PDOT
+    SC_LSPO, KC_NUHS, KC_Z    , KC_X   , KC_C    , KC_V  , KC_B  , KC_N  , KC_M   , KC_COMM, KC_DOT , KC_UP  , KC_SLSH, KC_P1  , KC_P2  , KC_P4  ,
+    KC_LCTL, KC_LGUI, TT(_LWR), KC_LALT, TT(_RSE), KC_SPC, KC_SPC, KC_SPC, KC_RALT, SC_RSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_APP , KC_P0  , KC_PDOT
 ),
 [_LWR] = LAYOUT_ansiortho_8x16(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL, _______, _______, _______,
