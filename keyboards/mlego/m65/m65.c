92196b888e7a2fb78fac279c130ca770092999de
Alin Marin Elena <alin@elena.space>
2023-04-02 09:37:58 +0100

    fix include

--- keyboards/mlego/m65/m65.c
+++ keyboards/mlego/m65/m65.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "m65.h"
+#include QMK_KEYBOARD_H
 
 void toggle_leds(const bool toggle_lwr, const bool toggle_rse) {
     led_lwr(toggle_lwr);
