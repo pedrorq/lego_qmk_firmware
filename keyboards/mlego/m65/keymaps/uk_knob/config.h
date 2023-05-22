5ddfa0cc0f963f4ebad65402acb7e4f13e3b6e80
alin elena <alin@elena.space>
2023-05-22 21:25:42 +0100

    update keymap unicodemap

--- keyboards/mlego/m65/keymaps/uk_knob/config.h
+++ keyboards/mlego/m65/keymaps/uk_knob/config.h
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define TAPPING_TOGGLE 2
+
 #define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_MACOS, UNICODE_MODE_WINDOWS, UNICODE_MODE_WINCOMPOSE
 
 #ifdef WPM_ENABLE
