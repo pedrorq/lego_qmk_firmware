71e1b2aa0929251220230b82f3b9b4ff21899b91
Alin Marin Elena <alin@elena.space>
2023-05-28 20:32:49 +0100

    update m60 rev2

--- keyboards/mlego/m60/m60.h
+++ keyboards/mlego/m60/m60.h
@@ -1,23 +1,27 @@
-/*
-Copyright 2021-2022 Alin M Elena <alinm.elena@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2021-2022 alin m elena (@alinelena)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include "quantum.h"
+
+enum layer_names { _QW = 0, _LWR, _RSE, _ADJ };
+
+#ifdef OLED_ENABLE
+void user_oled_magic(void);
+void render_logo(void);
+void clear_screen(void);
+void init_timer(void);
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+void                             set_rgb_layers(layer_state_t);
+const rgblight_segment_t* const* my_rgb(void);
+void                             set_default_rgb_layers(layer_state_t);
+#endif
+
+void toggle_leds(void);
+void set_led_toggle(const uint8_t, const bool);
 
 static inline void led_lwr(const bool on) {
 #ifdef LED_NUM_LOCK_PIN
@@ -32,7 +36,7 @@ static inline void led_rse(const bool on) {
 }
 static inline void led_caps(const bool on) {
 #ifdef LED_CAPS_LOCK_PIN
-        writePin(LED_CAPS_LOCK_PIN, !on);
+    // logic is inverse on original rpico
+    writePin(LED_CAPS_LOCK_PIN, on);
 #endif
 }
-
