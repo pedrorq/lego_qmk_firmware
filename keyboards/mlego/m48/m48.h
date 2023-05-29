9de80ed19331dd033ca3e94f383dd3580089c7f7
alin elena <alin@elena.space>
2023-05-29 13:09:37 +0100

    update m48 to use the userspace

--- keyboards/mlego/m48/m48.h
+++ keyboards/mlego/m48/m48.h
@@ -1,38 +1,36 @@
-/*
-Copyright 2021-2022 Alin M Elena <alinm.elena@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2021-2023 alin m elena (@alinelena)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include "quantum.h"
 
+enum layer_names { _QW = 0, _LWR, _RSE, _ADJ };
+
+void toggle_leds(const bool, const bool);
+
+static inline void init_lwr_rse_led(void) {
+#ifdef LED_LWR_PIN
+    setPinOutput(LED_LWR_PIN);
+    writePin(LED_LWR_PIN, false);
+    wait_ms(30);
+#endif
+
+#ifdef LED_RSE_PIN
+    setPinOutput(LED_RSE_PIN);
+    writePin(LED_RSE_PIN, false);
+    wait_ms(30);
+#endif
+}
+
 static inline void led_lwr(const bool on) {
-#ifdef LED_NUM_LOCK_PIN
-    writePin(LED_NUM_LOCK_PIN, on);
+#ifdef LED_LWR_PIN
+    writePin(LED_LWR_PIN, on);
 #endif
 }
 
 static inline void led_rse(const bool on) {
-#ifdef LED_SCROLL_LOCK_PIN
-    writePin(LED_SCROLL_LOCK_PIN, on);
+#ifdef LED_RSE_PIN
+    writePin(LED_RSE_PIN, on);
 #endif
 }
-static inline void led_caps(const bool on) {
-#ifdef LED_CAPS_LOCK_PIN
-    writePin(LED_CAPS_LOCK_PIN, !on);
-#endif
-}
-
--- keyboards/mlego/m65/keymaps/default/keymap.c
+++ keyboards/mlego/m65/keymaps/default/keymap.c
@@ -40,15 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_QW] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-#    ifdef RGBLIGHT_ENABLE
-    [_LWR] = {ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
-    [_RSE] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [_ADJ] = {ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)},
-#    else
     [_LWR] = {ENCODER_CCW_CW(KC_MNXT, KC_MPRV)},
     [_RSE] = {ENCODER_CCW_CW(KC_MFFD, KC_MRWD)},
     [_ADJ] = {ENCODER_CCW_CW(KC_PGDN, KC_PGUP)},
-#    endif
 };
 #endif
 
