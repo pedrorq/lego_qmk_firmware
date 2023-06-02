b37becc709c726c5534449bfe8ff5237877159be
alin m elena <alinm.elena@gmail.com>
2023-06-02 17:08:38 +0100

    update

---
+++ keyboards/mlego/m65/qp_config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define QUANTUM_PAINTER_NUM_DISPLAYS 1
+#define QUANTUM_PAINTER_NUM_IMAGES 16
+// might reduce this for non-surface setups
+#define QUANTUM_PAINTER_PIXDATA_BUFFER_SIZE 24000
+#define SURFACE_NUM_DEVICES 2
+#define QUANTUM_PAINTER_SUPPORTS_NATIVE_COLORS TRUE
