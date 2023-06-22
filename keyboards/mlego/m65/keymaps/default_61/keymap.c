c717efb02b209201fbc677567a8e2ac2b5b72b07
pedrorq <37935556+pedrorq@users.noreply.github.com>
2023-06-22 15:53:28 +0100

    Update keymap.c
    test

--- keyboards/mlego/m65/keymaps/default_61/keymap.c
+++ keyboards/mlego/m65/keymaps/default_61/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC, KC_GRV, KC_1   , KC_2    , KC_3   , KC_4    , KC_5  , KC_6  , KC_7  , KC_8   , KC_9   , KC_0   , KC_BSPC ,
        KC_TAB, KC_LALT,KC_Q   , KC_W    , KC_E   , KC_R    , KC_T  , KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_DEL,
       KC_LCTL, KC_MENU, KC_A   , KC_S    , KC_D   , KC_F    , KC_G  , KC_H  , KC_J  , KC_K   , KC_L  , KC_SCLN, KC_QUOT ,
-      SC_LSPO, KC_UP, KC_Z    , KC_X   , KC_C    , KC_V  , KC_B  , KC_N  , KC_M   , KC_COMM, KC_DOT , KC_SLSH , SC_RSPC,
+      KC_LSFT, KC_UP, KC_Z    , KC_X   , KC_C    , KC_V  , KC_B  , KC_N  , KC_M   , KC_COMM, KC_DOT , KC_SLSH , KC_RSFT,
       KC_LEFT, KC_DOWN, KC_RGHT, TT(_LWR), KC_SPC, KC_SPC, TT(_RSE), KC_SPC, KC_ENT),
   [_LWR] = LAYOUT_ortho_5x13_61(
        KC_GRV , KC_MUTE, KC_VOLU, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, G(KC_P), KC_SLEP, KC_WAKE, KC_PSCR, KC_DEL , KC_EQL  ,
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_BTN1, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______ ,
        _______, KC_BTN4, _______, _______, _______, _______,  KC_MS_L, KC_MS_D, KC_MS_R),
   [_RSE] = LAYOUT_ortho_5x13_61(
-      KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12  ,
+      KC_ESC , KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11  ,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,
       KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,
       _______, KC_WH_U, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,_______ ,
